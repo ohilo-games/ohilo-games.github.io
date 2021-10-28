b5f377eb0ff11015bc10b44f89454e88a17da0c8
Ayush-0801 <ayush19301@iiitd.ac.in>
2021-10-28 15:03:00 +0530

    Deleted Some More Files

--- src/screens/Intro.js
+++ src/screens/Intro.js
@@ -1,6 +1,5 @@
 import HeroVideo from '../components/HeroVideo';
 import Overlay from '../components/Overlay';
-import Logo from '../components/Logo';
 import Message from '../components/Message'
 import ArrowButton from '../components/ArrowButton';
 
